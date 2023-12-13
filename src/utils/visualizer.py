ea8f6a67d2ba7c15f4b987a66a92b9c41c091998
pauchai <paul.khimyack@gmail.com>
2023-12-13 06:12:54 +0000

    visualizer modified

--- src/utils/visualizer.py
+++ src/utils/visualizer.py
@@ -11,57 +11,116 @@ class Visualizer():
       axs[i].imshow(image)
     plt.show()
 
+  def check_data_batches(self, data_batch, image_batch):
+      def print_error(message):
+          print(f"Error: {message}")
+
+      #if not isinstance(data_batch, (list, tuple)):
+      #    print_error("Data must be a list or tuple.")
+      #    return False
+      if data_batch.shape[0] != image_batch.shape[0]:
+        print_error("Batches count data and images is not equal")
+        return False
+        
+      if not np.any(np.asarray(data_batch)):
+        print_error("Data is empty.")
+        return False
+
+
+      if len(data_batch) != len(image_batch):
+          print_error("Length mismatch: Data and image batch must have the same length.")
+          return False
+
+      return True
 
   def landmark_batch_image_joints_show(self, image_batch, landmark_batch):
-    fig, axs = plt.subplots(1, len(image_batch), figsize = (35,5))
+    try:
+      num_landmarks = len(landmark_batch[0])
+
+      if not self.check_data_batches(landmark_batch, image_batch):
+              return
+
+      fig, axs = plt.subplots(1, len(image_batch), figsize = (35,5))
+       # Check if axs is not iterable and convert it to a list
+      if not isinstance(axs, (list, np.ndarray)):
+          axs = np.array([axs])
+
+      for i, (image, landmarks) in enumerate(zip(image_batch, landmark_batch)):
+        if len(landmarks) != num_landmarks:
+                  print("Length mismatch: Landmarks and num_landmarks must have the same length.")
+                  continue
 
 
-    for i, (image, landmarks) in enumerate(zip(image_batch, landmark_batch)):
+        #рисуем сочленения
+        for  from_to_points in self.skeleton_graph:
+          landmark_from_idx = from_to_points[0]
+          landmark_to_idx = from_to_points[1]
 
+          x0, y0, _ = landmarks[landmark_from_idx]
+          x1, y1, _ = landmarks[landmark_to_idx]
+          # нарисовать линии между (x0,y0) (x1,x1)
+          axs[i].plot([x0, x1], [y0, y1], linewidth=2, color='r')
 
-      #рисуем сочленения
-      for  from_to_points in self.skeleton_graph:
-        landmark_from_idx = from_to_points[0]
-        landmark_to_idx = from_to_points[1]
+        # рисуем суставы
+        x, y, v = zip(*landmarks)
+        colors = ["r" if int(val) == 1 else  "b" for val in v] # 'r' for visible 1 'b' for 0
+        axs[i].scatter(x, y, c= colors, marker='o')  # , 'o' for circular marker
+        im = axs[i].imshow(image, vmin  = -2, vmax = 2)
+        cbar = fig.colorbar(im, ax=axs[i], shrink = 1.0)
 
-        x0, y0, _ = landmarks[landmark_from_idx]
-        x1, y1, _ = landmarks[landmark_to_idx]
-        # нарисовать линии между (x0,y0) (x1,x1)
-        axs[i].plot([x0, x1], [y0, y1], linewidth=2, color='r')
+      plt.show()
+    except Exception as e:
+      print(f"An error occurred: {e}") 
+      print(f"Shape of image_batch: {np.shape(image_batch)}")
+      print(f"Shape of heatmap_batch: {np.shape(landmark_batch)}")
 
-      # рисуем суставы
-      x, y, v = zip(*landmarks)
-      colors = ["r" if int(val) == 1 else  "b" for val in v] # 'r' for visible 1 'b' for 0
-      axs[i].scatter(x, y, c= colors, marker='o')  # , 'o' for circular marker
-      im = axs[i].imshow(image, vmin  = -2, vmax = 2)
-      cbar = fig.colorbar(im, ax=axs[i], shrink = 1.0)
-
-    plt.show()
 
   def heatmap_batch_vstacked_show(self,heatmap_batch):
-    fig, axs = plt.subplots(1, len(heatmap_batch), figsize = (25, 35))
-    heatmaps = None
-    for i, heatmap in enumerate(heatmap_batch):
-      heatmaps = np.transpose(heatmap, (2,0,1))
-      axs[i].imshow(np.vstack(heatmaps))
-    plt.show()
+    try:
+      fig, axs = plt.subplots(1, len(heatmap_batch), figsize = (25, 35))
+
+      # Check if axs is not iterable and convert it to a list
+      if not isinstance(axs, (list, np.ndarray)):
+          axs = np.array([axs])
+          
+      heatmaps = None
+      for i, heatmap in enumerate(heatmap_batch):
+        heatmaps = np.transpose(heatmap, (2,0,1))
+        axs[i].imshow(np.vstack(heatmaps))
+      plt.show()
+    except Exception as e:
+      print(f"An error occurred: {e}") 
+      print(f"Shape of heatmap_batch: {np.shape(heatmap_batch)}")
 
 
   def heatmap_batch_onimage_show(self,image_batch, heatmap_batch,  background_weight = 0.3, heatmap_weight = 0.8):
-    fig, axs = plt.subplots(1, len(heatmap_batch), figsize = (25, 35))
-    heatmaps = None
-    for i, (image, heatmap) in enumerate(zip(image_batch, heatmap_batch)):
-        bimg = image.copy()
+    #if not self.check_data_batches(heatmap_batch, image_batch):
+    #  return
 
-        axs[i].imshow(
-          self.draw_heatmap_on_image(bimg, heatmap.copy(), background_weight = background_weight, heatmap_weight = heatmap_weight),
-          vmin = -2,
-          vmax = 2
+    try:
+      fig, axs = plt.subplots(1, len(heatmap_batch), figsize = (25, 35))
+      
+      # Check if axs is not iterable and convert it to a list
+      if not isinstance(axs, (list, np.ndarray)):
+          axs = np.array([axs])
 
-          
-        )
+      heatmaps = None
+      for i, (image, heatmap) in enumerate(zip(image_batch, heatmap_batch)):
+          bimg = image.copy()
 
-    plt.show()
+          axs[i].imshow(
+            self.draw_heatmap_on_image(bimg, heatmap.copy(), background_weight = background_weight, heatmap_weight = heatmap_weight),
+            vmin = -2,
+            vmax = 2
+
+            
+          )
+
+      plt.show()
+    except Exception as e:
+      print(f"An error occurred: {e}") 
+      print(f"Shape of image_batch: {np.shape(image_batch)}")
+      print(f"Shape of heatmap_batch: {np.shape(heatmap_batch)}")
 
   def draw_heatmap_on_image(self, img, heatmap, background_weight = 0.3, heatmap_weight = 0.8):
     if (img.shape[-2] != heatmap.shape[-2] ):
